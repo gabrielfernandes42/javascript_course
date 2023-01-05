783658c030d6c1b82c8ba4ddf43035918287818d
gabriel <gabrielfernandes254@gmail.com>
2023-01-05 17:05:03 -0300

    Math methods

---
+++ 04_lesson/js/main.js
@@ -0,0 +1,11 @@
+// math methods and properties
+
+// console.log(Math.PI);
+// console.log(Math.trunc(Math.PI));
+// console.log(Math.floor(3.9));
+// console.log(Math.pow(2, 9));
+// console.log(Math.min(2, 0.5, 9));
+// console.log(Math.random());
+
+//from 1 to 10
+console.log(Math.floor(Math.random() * 10 + 1));
