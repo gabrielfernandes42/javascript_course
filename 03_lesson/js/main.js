346abb6aba79676c7645a7b5eb6fac88032e407c
gabriel <gabrielfernandes254@gmail.com>
2023-01-05 16:41:22 -0300

    Numbers

---
+++ 03_lesson/js/main.js
@@ -0,0 +1,19 @@
+//Numbers
+
+//An integer is a whole number
+const myNumber = 42;
+
+// A number with a decimal point is a float witch references the "Floating pont"
+
+const myFloat = 42.02;
+const myString  = "42"
+
+//Number methods
+//The number .isInsteger() method determines wether the passed value is an integer
+
+//The number .parseInt() method parses a string argument and return an integer
+
+
+//Chaining = using several methods chaided togther
+
+console.log(myNumber === myString);
