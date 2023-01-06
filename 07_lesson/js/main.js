6d7b9000e67fe9f7eb416af211ba1604584067fe
gabriel <gabrielfernandes254@gmail.com>
2023-01-06 15:57:00 -0300

    Switch

---
+++ 07_lesson/js/main.js
@@ -0,0 +1,39 @@
+// Conditionals: Swotch statements
+
+//syntax
+
+// switch (expression or value) {
+//   case  choice1:
+//     //run this code
+//     break;
+//   case  choice2:
+//     //run this code
+//     break;
+
+//     //add as many cases as needed
+//   default:
+//   //run this code if no cases matches
+//   //no need for break here
+//
+
+switch (2) {
+  case 1:
+    //run this code
+    console.log(1);
+
+    break;
+  case 2:
+    //run this code
+    console.log(2);
+
+    break;
+  case 3:
+    //run this code
+    console.log(3);
+
+    break;
+
+  //add as many cases as needed
+  default:
+    console.log("no match! ");
+}
