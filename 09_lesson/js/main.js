5465b20534ec633ddec461c85193cb8332d25262
gabriel <gabrielfernandes254@gmail.com>
2023-01-06 18:51:19 -0300

    User input

---
+++ 09_lesson/js/main.js
@@ -0,0 +1,14 @@
+//User input
+
+// alert("Hello World! ")
+// let myBooleand = confirm("ok === True\nCancel == False")
+// console.log(myBooleand);
+
+let names = prompt("Please enter your name")
+
+if(names) {
+
+    console.log(names)
+}else {
+    prompt("Please, type yout name!")
+}
