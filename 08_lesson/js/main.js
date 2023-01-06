d1c81cf5f9cb3763f1cd90bdefcd88818ddf1620
gabriel <gabrielfernandes254@gmail.com>
2023-01-06 16:10:15 -0300

    Ternary Operators

---
+++ 08_lesson/js/main.js
@@ -0,0 +1,9 @@
+//Conditionals: Ternary Operator
+
+//syntax
+//condition ? ifTrue : ifFalse
+
+let soup = "Chichen Noodle Soup";
+let response = !soup ? "Yes, we have soup" : "Sorry, no soup today"
+
+console.log(response)
