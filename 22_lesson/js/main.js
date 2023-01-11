ff0f638e940409ff1231abb917e1c4b4d8d70658
gabriel <gabrielfernandes254@gmail.com>
2023-01-11 19:35:12 -0300

    Web Storage API

---
+++ 22_lesson/js/main.js
@@ -0,0 +1,21 @@
+// Web Storage API
+
+//not part of the DOM - refers to the Window API
+//Available to JS via the global variabel: window
+
+//we do not have to type window. It if implid
+
+const myArray = ["eat", "sleep", "code"];
+const myObjt = {
+  name: "Gabriel",
+  hobbiews: ["eat", "sleep", "code"],
+  logName: function () {
+    console.log(this.name);
+  },
+};
+
+sessionStorage.setItem("mySessionstore", JSON.stringify(myArray));
+
+const mySessionData = JSON.parse(sessionStorage.getItem("mySessionstore"));
+
+console.log(mySessionData);
